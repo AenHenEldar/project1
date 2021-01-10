87d1fdab6229849cef9e60c8964c2ba27afbdd6c
Eldar <eldar.hidirow@gmail.com>
2021-01-11 00:40:04 +0300

    Mod js. Added prompts and objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 /* jslint node: true */
 "use strict";
 
-const myName = 'Eldar';
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-alert(`Hello, ${myName}`);
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let movie = prompt("Один из последних просмотренных фильмов?", "");
+let movieRate = prompt("На сколько оцените его?", "");
+
+personalMoviesDB.movies[movie] = movieRate;
+
+movie = prompt("Один из последних просмотренных фильмов?", "");
+movieRate = prompt("На сколько оцените его?", "");
+
+personalMoviesDB.movies[movie] = movieRate;
+
+console.log(personalMoviesDB);
