af1eb38b2f76bf120412b3b8d0c07c3b669392bd
Eldar <eldar.hidirow@gmail.com>
2021-01-11 13:50:07 +0300

    Lesson №15

--- js/script.js
+++ js/script.js
@@ -11,14 +11,27 @@ const personalMoviesDB = {
     privat: false
 };
 
-let movie = prompt("Один из последних просмотренных фильмов?", "");
-let movieRate = prompt("На сколько оцените его?", "");
+for(let i = 0; i < 2; i++) {
+    const movie = prompt("Один из последних просмотренных фильмов?", ""),
+          movieRate = +prompt("На сколько оцените его?", "");
 
-personalMoviesDB.movies[movie] = movieRate;
+    if(movie && movieRate && movie != "" && movie.length < 50) {
+        personalMoviesDB.movies[movie] = movieRate;
+    } else {
+        console.log("error");
+        i--;
+    }
+    
+}
 
-movie = prompt("Один из последних просмотренных фильмов?", "");
-movieRate = prompt("На сколько оцените его?", "");
-
-personalMoviesDB.movies[movie] = movieRate;
+if(personalMoviesDB.count < 10 && personalMoviesDB.count) {
+    alert('Просмотрено довольно мало фильмов');
+} else if(personalMoviesDB.count < 30 && personalMoviesDB.count) {
+    alert('Вы классический зритель');
+} else if(personalMoviesDB.count >= 30 && personalMoviesDB.count) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(personalMoviesDB);
